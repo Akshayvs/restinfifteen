3f5c2f7cd8322fe8cb6683dc32c98f28b15cbea6
Akshay Sonawane <akshay.vs9543@gmail.com>
2021-03-16 07:35:39 -0700

    init

---
+++ src/main/java/com/example/restinfifteen/RestinfifteenApplication.java
@@ -0,0 +1,13 @@
+package com.example.restinfifteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestinfifteenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestinfifteenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restinfifteen/RestinfifteenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restinfifteen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestinfifteenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
